6b23dccd3d54dea64a7479ecc4e022761e845e40
eseffair <eseffair@gmail.com>
2013-12-06 08:20:56 +0000

    Segundo sprint

--- eventex/settings.py
+++ eventex/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # os
 #BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from dj_database_url import parse as db_url
 from decouple import config
 from unipath import Path
 BASE_DIR = Path(__file__).parent
